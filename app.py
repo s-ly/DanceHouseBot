7144097741accb98f1ffb345d335671eaeeeaf3b
s-ly <sergeylysov@outlook.com>
2021-11-30 22:22:46 +0300

    Добавил машину состояний для админки

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 # Телеграм бот t.me/DanceHouseBot
 # Школа танцев.
 
+from re import X
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import state
 import MyToken  # содержит токен
+
+# Машина состояний
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+# хранение контекста
+from aiogram.dispatcher import FSMContext 
+
+# место для хранения контекста в ОЗУ
+from aiogram.contrib.fsm_storage.memory import MemoryStorage 
 
 # для кнопок
 from aiogram.types.reply_keyboard import KeyboardButton, ReplyKeyboardMarkup
@@ -28,8 +39,14 @@ API_TOKEN = MyToken.myToken # рабочий бот
 # API_TOKEN = MyToken.testToken # тестовый бот
 
 # Initialize bot and dispatcher
+storage = MemoryStorage() # место хранения контекста в ОЗУ
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
+
+# создаём состояния
+class StateGroupFSM(StatesGroup):
+    user_state_admin = State()  # для админки
+    user_state_default = State()  # все остальные
 
 # создаём коавиатуру и добавляем кнопки
 # resize_keyboard=True - уменьшает кнопки
@@ -43,10 +60,53 @@ button_contact = KeyboardButton('Контакты')
 button_timetable = KeyboardButton('Расписание')
 keyboard.add(button_about, button_price, button_contact, button_timetable)
 
+# Клавиатура для админки
+keyboard_admin = ReplyKeyboardMarkup(resize_keyboard=True)
+button_exit_admin = KeyboardButton('Выход из админки')
+keyboard_admin.add(button_exit_admin)
 
 
 
-@dp.message_handler(Text(equals="Расписание"))
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    """ Отвечает на команду: /start когда состояние пользователя не установлено.
+    Устанавливает состояние пользователя на user_state_default."""
+    await StateGroupFSM.user_state_default.set()
+    await message.answer(Texts.text_start, reply_markup=keyboard)    
+
+
+
+
+@dp.message_handler(commands=['gs'], state='*')
+async def get_state(message: types.Message, state: FSMContext):
+    """ Узнать текущий статус состояния"""
+    cs = await state.get_state()
+    print(cs)
+
+
+
+
+@dp.message_handler(Text(equals="Выход из админки"), state=StateGroupFSM.user_state_admin)
+async def buttonTimetableMess(message: types.Message):
+    """ Отвечает на кнопку: button_exit_admin, если состояние прльзователя admin.
+    Потом переводит состояние пользователя в default."""
+    await StateGroupFSM.user_state_default.set()
+    await message.answer('Выход из админки', reply_markup=keyboard)
+
+
+
+
+@dp.message_handler(commands=['admin'], state=StateGroupFSM.user_state_default)
+async def buttonTimetableMess(message: types.Message, state: FSMContext):
+    """ Отвечает на команду: /admin"""
+    # await StateGroupFSM.user_state_admin.set()
+    await func.runAdmin(message, keyboard_admin)
+
+
+
+
+@dp.message_handler(Text(equals="Расписание"), state=StateGroupFSM.user_state_default)
 async def buttonTimetableMess(message: types.Message):
     """ Отвечает на кнопку: button_timetable"""
     photo = func.getPhoto()
@@ -56,7 +116,7 @@ async def buttonTimetableMess(message: types.Message):
 
 
 
-@dp.message_handler(Text(equals="О нас"))
+@dp.message_handler(Text(equals="О нас"), state=StateGroupFSM.user_state_default)
 async def buttonAboutMess(message: types.Message):
     """ Отвечает на кнопку: utton_about"""
     # reply_markup=keyboard - показывает клавиатуру
@@ -65,18 +125,17 @@ async def buttonAboutMess(message: types.Message):
 
 
 
-@dp.message_handler(Text(equals="Прайс-лист"))
+@dp.message_handler(Text(equals="Прайс-лист"), state=StateGroupFSM.user_state_default)
 async def buttonPriceMess(message: types.Message):
     """ Отвечает на кнопку: utton_about"""
     # reply_markup=keyboard - показывает клавиатуру
     # code(...) и parse_mode=... форматируют текст моноширно
-
     await message.answer(code(Texts.text_price), parse_mode=types.ParseMode.MARKDOWN_V2, reply_markup=keyboard)
 
 
 
 
-@dp.message_handler(Text(equals="Контакты"))
+@dp.message_handler(Text(equals="Контакты"), state=StateGroupFSM.user_state_default)
 async def buttonContactMess(message: types.Message):
     """ Отвечает на кнопку: utton_about"""
     # reply_markup=keyboard - показывает клавиатуру
@@ -85,12 +144,30 @@ async def buttonContactMess(message: types.Message):
 
 
 
-@dp.message_handler()
-async def allMess(message: types.Message):
-    """ Отвечает на любые сообщения."""
+
+@dp.message_handler(state=StateGroupFSM.user_state_default)
+async def all_mess_state_default(message: types.Message):
+    """ Отвечает на любые сообщения, если состояние не default"""
     # reply_markup=keyboard - показывает клавиатуру
     await message.answer('Используйте кнопки.', reply_markup=keyboard)
 
+
+
+
+@dp.message_handler()
+async def all_mess_state_default(message: types.Message):
+    """ Отвечает на любые сообщения, если состояние не default"""
+    await message.answer('Переброска на Start')
+    await start(message)
+
+
+
+
+@dp.message_handler(state=StateGroupFSM.user_state_admin)
+async def allMess(message: types.Message):
+    """ Отвечает на любые сообщения в админке.
+    Перехватывает состояние user_state_admin."""
+    await message.answer('Используйте кнопки для админки.')
 
 
 
--- func.py
+++ func.py
@@ -1,8 +1,44 @@
 # Фукции для бота
+# from aiogram.dispatcher.filters import state
 
-# from aiogram import types
+from aiogram import Bot, Dispatcher, executor, types
+
+# Машина состояний
+# from aiogram.dispatcher.filters.state import State, StatesGroup
+
+# хранение контекста
+# from aiogram.dispatcher import FSMContext 
+
+# место для хранения контекста в ОЗУ
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+# Мой класс для работы с состояниями пользователя
+from app import StateGroupFSM 
+
+
+
 
 def getPhoto():    
     """возвращает фото расписания занятий"""
     photo = open('data/novoe_raspisanie.jpg', 'rb')
     return photo
+
+
+
+
+# Осуществляет проверку id пользователя.
+# Если id = админу, то включает админку.
+async def runAdmin(message: types.Message, keyboard_admin):
+    """ Запуск админки.
+    Осуществляет проверку id пользователя.
+    Если id = админу, то переводит состояние пользователя в состояние admin
+    и переключает на админ-клавиатуру."""
+    admin_id_lysov = 80315171  # мой id
+    admin_id_vladimir = 434967278 # id Владимира
+    user_id = message.from_user.id  # узнать id пользователя
+    print(user_id)
+    if (user_id == admin_id_lysov or user_id == admin_id_vladimir):
+        await StateGroupFSM.user_state_admin.set()
+        await message.answer('Вход в админку', reply_markup=keyboard_admin)
+    else:
+        await message.answer('Только для админов')
