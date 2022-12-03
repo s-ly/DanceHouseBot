df9e49e353582b93cb8a878c5880a4a35ce49e68
s-ly <SergeyLysov@outlook.com>
2022-12-03 21:38:25 +0300

    v1.3

--- admin.py
+++ admin.py
@@ -256,5 +256,5 @@ def questionnaires():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='81.163.31.153', port=5001)
-    # app.run(debug=True, host='0.0.0.0')
+    # app.run(debug=True, host='81.163.31.153', port=5001)
+    app.run(debug=True, host='0.0.0.0')
